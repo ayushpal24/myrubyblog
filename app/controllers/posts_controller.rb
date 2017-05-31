f25d302bc5c9efd8d78da86fba1b026a5a28afb0
ayushpal24 <om112911@gmail.com>
2017-05-31 18:56:03 +0530

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,58 @@
+class PostsController < ApplicationController
+  def index
+   @posts= Post.all
+  end
+
+  def show
+   @post= Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+    @category= Category.all
+  end
+
+  # def create
+  #    @post= Post.new(params[:post])
+  #    if @post.save
+  #      redirect_to post_path(@post),:notice => "Your post has been saved"
+  #    else
+  #      render 'new'
+  #    end
+   #end
+
+ def create
+    @post = Post.new(permit_post)
+    if @post.save
+      redirect_to post_path(@post),:notice => "Your post has been saved"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post= Post.find(params[:id])
+  end
+
+  def update
+       @post= Post.find(params[:id])
+       if @post.update_attributes(permit_post)
+        redirect_to post_path(@post),:notice => "Your post has been updated"
+      else
+        render 'edit'
+      end
+  end
+
+  def destroy
+  @post= Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path,:notice => "Your post has been deleted"
+  end
+
+
+  private
+
+      def permit_post
+        params.require(:post).permit(:title,:body,:category_id)
+      end
+  end
